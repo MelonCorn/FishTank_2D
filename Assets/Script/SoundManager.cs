ae7f5387ce04aa71ac1bcd8f72ebf18860d18b12
MelonCorn <rhfoem456@gmail.com>
2025-11-27 00:39:35 +0900

    [feat] 배경음 출력
    배경음 목록 순차대로 재생하는 코루틴 실행

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -1,12 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
-using static UnityEngine.Rendering.DebugUI;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
     [SerializeField] AudioMixer mixer;      // 믹서
+
+    [SerializeField] AudioSource bgmSource; // 배경음 소스
+    [SerializeField] AudioSource sfxSource; // 효과음 소스
+
+    [SerializeField] AudioClip[] bgms;      // 배경음악 리스트
+
+    WaitUntil bgmStop;                      // 배경음 재생 코루틴 조건
+    private int currentBgmIndex = 0;        // 현재 재생중인 배경음
+
 
     // 현재 볼륨
     private float masterVolume = 0.5f;
@@ -29,7 +38,18 @@ public class SoundManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+        // 볼륨 불러오기
         LoadVolumePrefs();
+        // 배경음 재생 조건
+        bgmStop = new WaitUntil(() => bgmSource.isPlaying == false);
+    }
+
+
+    private void Start()
+    {
+        // 배경음 목록 있고, 소스 있으면
+        if (bgms.Length <= 0 || bgmSource == null) return;
+            StartCoroutine(PlayBGM());
     }
 
     // 볼륨 불러오기
@@ -72,5 +92,25 @@ public class SoundManager : MonoBehaviour
         sfxVolume = value;
         mixer.SetFloat("SFX", Mathf.Log10(value) * 20);
         PlayerPrefs.SetFloat("SFX", value);
+    }
+
+
+    // 배경음 재생
+    IEnumerator PlayBGM()
+    {
+        // 무한 반복
+        while (true)
+        {
+            // 멈출 때 까지 기다림
+            yield return bgmStop;
+
+            // 갈아 끼우고
+            bgmSource.clip = bgms[currentBgmIndex];
+            // 재생
+            bgmSource.Play();
+
+            // 다음 번호
+            currentBgmIndex = (currentBgmIndex + 1) % bgms.Length;
+        }
     }
 }
